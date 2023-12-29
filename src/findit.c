db36fefbdd0b347e2435611ccdd2bb6d9123d75b
BiltosLab <mrlaith44@gmail.com>
2023-12-29 15:26:01 +0300

    fixed some bugs

--- src/findit.c
+++ src/findit.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+// #define PATH_MAX 1024 * 1024
 
 void parsedirs(char *dirname, char *searchkey)
 {
@@ -15,43 +18,69 @@ void parsedirs(char *dirname, char *searchkey)
     {
         while ((dir = readdir(d)) != NULL)
         {
+            char Foundaddr[PATH_MAX];
+            char fullPath[PATH_MAX];
+            char resolvedPath[PATH_MAX];
             char *stream = dir->d_name;
-            if (strcmp(stream, ".") == 0 || strcmp(stream, "..") == 0)
+            snprintf(fullPath, sizeof(fullPath), "%s/%s", dirname, stream);
+            realpath(fullPath, resolvedPath); // Use realpath to handle symbolic links
+            
+
+
+
+            if (strcmp(stream, ".") == 0 || strcmp(stream, "..") == 0 || strncmp(stream, ".", 1) == 0 )
             {
                 continue;
             }
 
-            char fullPath[10240];
-            snprintf(fullPath, sizeof(fullPath), "%s/%s", dirname, stream);
-
-            if (stat(fullPath, &fileInfo) == 0)
+            if (stat(resolvedPath, &fileInfo) == 0)
             {
+                if (S_ISLNK(fileInfo.st_mode))
+                {
+                    
+                    printf("ENOUGHHHH!");
+                    continue;
+                }
                 if (S_ISDIR(fileInfo.st_mode))
                 {
-                    if (strstr(stream, searchkey)!=NULL)
+                    if (strstr(stream, searchkey) != NULL)
                     {
                         printf("Found %s at \t", searchkey);
                         printf("%s\n", fullPath);
+                        snprintf(Foundaddr, sizeof(Foundaddr), "%s", fullPath);
+                        //printf("FOUNDADDR %s\n", Foundaddr);
                     }
+                    else if (strstr(Foundaddr, fullPath) != NULL)
+                    {
+                        printf("Was found");
+                        continue;
+                    }
+
                     parsedirs(fullPath, searchkey);
                 }
                 else if (S_ISREG(fileInfo.st_mode))
                 {
 
-                    if (strstr(stream, searchkey)!=NULL)
+                    if (strstr(stream, searchkey) != NULL)
                     {
                         printf("Found %s at \t", searchkey);
                         printf("%s\n", fullPath);
+                        snprintf(Foundaddr, sizeof(Foundaddr), "%s", fullPath);
+                      //  printf("FOUNDADDR %s\n", Foundaddr);
+                    }
+                    else if (strstr(Foundaddr, fullPath) != NULL)
+                    {
+                        printf("Was found");
+                        continue;
                     }
                 }
                 else
                 {
-                    if (strstr(stream, searchkey)!=NULL)
+                    if (strstr(stream, searchkey) != NULL)
                     {
-                        printf("Found %s at \t\n But this(%s)is neither a directory nor a regular file.\n", searchkey,stream);
+                        printf("Found %s at \t\n But this(%s)is neither a directory nor a regular file.\n", searchkey, stream);
                         printf("%s\n", fullPath);
                     }
-                    
                 }
             }
             else
